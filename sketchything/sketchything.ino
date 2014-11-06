572f630e4286cc6a0d45acb5440d6bbf180571fe
Vaccuss <Deansteven308@gmail.com>
2014-11-07 00:52:15 +1000

    Getting sensors to work
    DHT11 - Humidity is working
    Light - is working
    Tempreture - is working
    
    **Code only if I bump a wire then I am still screwed**

--- sketchything/sketchything.ino
+++ sketchything/sketchything.ino
@@ -1,23 +1,38 @@
-
-#include <BH1750.h>
-#include <Wire.h>
 #include <DHT.h>
+#include <BH1750.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
-
+OneWire onewire(6);
+DallasTemperature sensor(&onewire);
 BH1750 lightMeter;
-#define DHT11_PIN 6
-
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup(){
   Serial.begin(9600);
   lightMeter.begin();
   Serial.println("Test god damn test");
+  sensor.begin();
+  dht.begin();
+  
 }
 
 void loop(){
-int lux = lightMeter.readLightLevel();
-Serial.println(lux);
-
+  delay(500);
+  uint16_t lux = lightMeter.readLightLevel();
+  float humid = dht.readHumidity();
+  Serial.print("Humidity: ");
+  Serial.println(humid);
+  Serial.print("The lux is: ");
+  Serial.println(lux);
+  sensor.requestTemperatures();
+  
+  Serial.print("Temp: ") ;
+  Serial.println(sensor.getTempCByIndex(0));
+  
 
 
 
